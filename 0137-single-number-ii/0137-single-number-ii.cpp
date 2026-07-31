248dafefdc4fd58b7aa4bd630429593e3cfb7da3
Tanishka Batham <bathamtanishka639@gmail.com>
2026-07-31 23:51:30 +0530

    Time: 0 ms (100%), Space: 13.3 MB (87.45%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.cpp
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -4,7 +4,7 @@ public:
         
         sort(nums.begin(), nums.end());
 
-        for (int i = 0; i < nums.size() - 1; i += 3) {
+        for (int i = 0; i < nums.size() - 1; i +=3) {
             if (nums[i] != nums[i + 1])
                 return nums[i];
         }
